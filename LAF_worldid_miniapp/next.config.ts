941c914bdc9d6b50902dc95f5f64efb5784bfea8
Michael Rizhakov <m.rizhakov@gmail.com>
2025-07-05 03:22:49 +0200

    Static grok working :

--- LAF_worldid_miniapp/next.config.ts
+++ LAF_worldid_miniapp/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["static.usernames.app-backend.toolsforhumanity.com"],
   },
-  allowedDevOrigins: ["aardvark-careful-purely.ngrok-free.app"], // Add your dev origin here
+  allowedDevOrigins: ["https://aardvark-careful-purely.ngrok-free.app"], // Add your dev origin here
   reactStrictMode: false,
 };
 
