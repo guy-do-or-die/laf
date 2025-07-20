3d1f709faf66e26387577eeeb6145aacf369e5b9
Egor Gusev <guy.do.or.die@gmail.com>
2025-07-20 19:47:31 +0400

    Fix build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+import react from "@vitejs/plugin-react";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
